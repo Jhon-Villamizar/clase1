2c8008e8b07c275202e38d7d8035b2bc48a0d068
Jhon Villamizar <jhonvio@hotmail.com>
2020-09-02 23:23:23 -0500

    changes for second video

--- clase2/src/App.js
+++ clase2/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//!JSX
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Clase # 2
+          Clase # 2!!!!!!!!!
         </p>
         <a
           className="App-link"
