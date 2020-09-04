23e0850dd6e42420e3844e16d9faf7f9d6a93c51
Jhon Villamizar <jhonvio@hotmail.com>
2020-09-03 23:01:54 -0500

    primera practica navegacion

--- clase2/src/App.js
+++ clase2/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './components/nav';
+
 //!JSX
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Clase # 2!!!!!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Clase donde creamos nuestra app React y estudiamos su estructura
-        </a>
-      </header>
+    <div>
+      <Nav />
     </div>
   );
 }
